e9fd27ba91aed8c071349e87696f70f0804515d8
Antonio Cheltenham <antoniocheltenham@Antonios-MacBook-Pro-3.local>
2022-10-28 15:13:12 -0400

    add buildpack 12

--- pub_sub_write/main.py
+++ pub_sub_write/main.py
@@ -1,40 +1,16 @@
-import base64
-import json
-import os
-
-from google.cloud import pubsub_v1
-
-
-# Instantiates a Pub/Sub client
-publisher = pubsub_v1.PublisherClient()
-PROJECT_ID = os.getenv('GOOGLE_CLOUD_PROJECT')
-
-
-# Publishes a message to a Cloud Pub/Sub topic.
-def publish(request):
-    request_json = request.get_json(silent=True)
-
-    topic_name = request_json.get("topic")
-    message = request_json.get("message")
-
-    if not topic_name or not message:
-        return ('Missing "topic" and/or "message" parameter.', 400)
-
-    print(f'Publishing message to topic {topic_name}')
-
-    # References an existing topic
-    topic_path = publisher.topic_path(PROJECT_ID, topic_name)
-
-    message_json = json.dumps({
-        'data': {'message': message},
-    })
-    message_bytes = message_json.encode('utf-8')
-
-    # Publishes a message
-    try:
-        publish_future = publisher.publish(topic_path, data=message_bytes)
-        publish_future.result()  # Verify the publish succeeded
-        return 'Message published.'
-    except Exception as e:
-        print(e)
-        return (e, 500)
+def hello_world(request):
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
